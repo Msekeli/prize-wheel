c319b71f27ec8709f4ffbb3fd1964349e0d820a0
Msekeli <msekeli14@gmail.com>
2023-12-21 14:24:39 +0200

    updated html and js files

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const wheel = document.getElementById("wheel");
 const spinBtn = document.getElementById("spin-btn");
 const finalValue = document.getElementById("final-value");
-//Object that stores values of minimum and maximum angle for a value
 const rotationValues = [
   { minDegree: 0, maxDegree: 30, value: 2 },
   { minDegree: 31, maxDegree: 90, value: 1 },
@@ -11,22 +10,15 @@ const rotationValues = [
   { minDegree: 271, maxDegree: 330, value: 3 },
   { minDegree: 331, maxDegree: 360, value: 2 },
 ];
-//Size of each piece
 const data = [16, 16, 16, 16, 16, 16];
-//background color for each piece
 var pieColors = [
   "#9336B4", 
 ];
-//Create chart
 let myChart = new Chart(wheel, {
-  //Plugin for displaying text on pie chart
   plugins: [ChartDataLabels],
-  //Chart Type Pie
   type: "pie",
   data: {
-    //Labels(values which are to be displayed on chart)
-    labels: ["$10","$20", "$30", "$40", "$50", "$60"],
-    //Settings for dataset/pie
+    labels: data,
     datasets: [
       {
         backgroundColor: pieColors,
@@ -35,16 +27,13 @@ let myChart = new Chart(wheel, {
     ],
   },
   options: {
-    //Responsive chart
     responsive: true,
     animation: { duration: 0 },
     plugins: {
-      //hide tooltip and legend
       tooltip: false,
       legend: {
         display: false,
       },
-      //display labels inside pie chart
       datalabels: {
         color: "#ffffff",
         formatter: (_, context) => context.chart.data.labels[context.dataIndex],
@@ -53,40 +42,25 @@ let myChart = new Chart(wheel, {
     },
   },
 });
-// display value based on the randomAngle
 const valueGenerator = (angleValue) => {
   for (let i of rotationValues) {
-    // if the angleValue is between min and max then display it
     if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
-      const wonAmount = i.value * 10; // Assuming each value corresponds to $10
+      const wonAmount = i.value * 10;
       finalValue.innerHTML = `<p>Congratulations! You won $${wonAmount}</p>`;
       spinBtn.disabled = false;
       break;
     }
   }
 };
-
-//Spinner count
 let count = 0;
-//100 rotations for animation and last rotation for result
 let resultValue = 101;
-//Start spinning
 spinBtn.addEventListener("click", () => {
   spinBtn.disabled = true;
-  //Empty final value
-  finalValue.innerHTML = `<p>Good Luck!</p>`;
-  //Generate random degrees to stop at
+  finalValue.innerHTML = `<p>let's go!</p>`;
   let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
-  //Interval for rotation animation
   let rotationInterval = window.setInterval(() => {
-    //Set rotation for piechart
-    /*
-    Initially to make the piechart rotate faster we set resultValue to 101 so it rotates 101 degrees at a time and this reduces by 1 with every count. Eventually on last rotation we rotate by 1 degree at a time.
-    */
     myChart.options.rotation = myChart.options.rotation + resultValue;
-    //Update chart with new value;
     myChart.update();
-    //If rotation>360 reset it back to 0
     if (myChart.options.rotation >= 360) {
       count += 1;
       resultValue -= 5;
@@ -99,3 +73,21 @@ spinBtn.addEventListener("click", () => {
     }
   }, 10);
 });
+
+async function getWheelValues() {
+  try {
+    const response = await fetch('http://localhost:7071/api/getwheelvalues', {
+      method: 'get',
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    myChart.data.labels = data;
+    myChart.update();
+  } catch (error) {
+    console.error(error);
+  }
+}

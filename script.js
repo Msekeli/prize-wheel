19514af96d25d93bc7e2a9adfb50c480f5242eee
Msekeli <msekeli14@gmail.com>
2024-01-30 16:12:29 +0200

    Function to generate a random GUID for userId

--- script.js
+++ script.js
@@ -58,6 +58,15 @@ const valueGenerator = (angleValue, wheelData) => {
 let count = 0;
 let resultValue = 101;
 
+// Function to generate a random GUID for userId
+const generateRandomGuid = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0,
+        v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+};
+
 // Function to update the wheel with values from the API
 const updateWheelValues = async () => {
   await getWheelValues();
